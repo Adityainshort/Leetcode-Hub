f5f2d6fa5d4efbb3b1a4246ffdc21e2d086e1877
Aditya Raj Singh <adityarajsinghhardoi@gmail.com>
2023-11-03 15:27:12 +0530

    updated leetcode0242

---
+++ Leetcode0242/leetcode0242.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      
+      
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+        int i=0 ;
+        while (i<sa.length){
+            if(sa[i]==ta[i]){
+                i++;
+                continue ;
+            }
+            else{
+                return false ;
+            }
+        }
+        return true ;
+
+    }
+}
