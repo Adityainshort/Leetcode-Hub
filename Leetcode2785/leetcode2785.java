f0bd4e78e7d98092cc7299b322310c7ff4dcb05b
Aditya Raj Singh <adityarajsinghhardoi@gmail.com>
2023-11-13 14:22:26 +0530

    added 2785 13thnov

---
+++ Leetcode2785/leetcode2785.java
@@ -0,0 +1,34 @@
+package Leetcode2785;
+
+import java.util.Arrays;
+
+class Solution {
+    public static String sortVowels(String s) {
+        int j = 0 ;
+        for (int i = 0; i < s.length(); i++) {
+            if ("aeiouAEIOU".contains(String.valueOf(s.charAt(i)))) {
+                j++;
+            }
+            
+        }
+        char[] arr = new char[j];
+        j=0;
+        for (int i = 0; i < s.length(); i++) {
+            if ("aeiouAEIOU".contains(String.valueOf(s.charAt(i)))) {
+                arr[j] = s.charAt(i);
+                j++;
+            }
+        }
+        Arrays.sort(arr);
+        j=0;
+        StringBuilder ans = new StringBuilder(s);
+        for (int i = 0; i < s.length(); i++) {
+            if ("aeiouAEIOU".contains(String.valueOf(s.charAt(i)))) {
+                ans.setCharAt(i,arr[j]);
+                j++;
+            }
+        }
+
+        return ans.toString() ;
+    }
+}
