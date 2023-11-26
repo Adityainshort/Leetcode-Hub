1c447b61fcc2cf400d0ee5ab547f90731d7094c3
Aditya Raj Singh <adityarajsinghhardoi@gmail.com>
2023-11-26 23:46:31 +0530

    added 0015 26th nov

--- Leetcode0015/leetcode15.java
+++ Leetcode0015/leetcode15.java
@@ -1,43 +1,38 @@
-
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * leetcode15
- */
-public class leetcode15 {
+class Solution {
 
-    public static List<List<Integer>> threeSum(int[] nums) {
+   public static   List<List<Integer>> threeSum(int[] nums) {
         int n= nums.length;
-        List<List<Integer>> listOfLists = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> uniq = new HashSet<>();
+
         for(int i= 0;i<n;i++){
+            Set<Integer> set = new HashSet<>();
             for(int j=i+1; j<n;j++){
-                for(int k=j+1; k<n;k++){
-                    if( nums[i] + nums[j] + nums[k] ==0){
-                        List<Integer> innerList1 = new ArrayList<>();
-                            innerList1.add(nums[i]);
-                            innerList1.add(nums[j]);
-                            innerList1.add(nums[k]);
-                        listOfLists.add(innerList1);
+                if(set.contains(-nums[i]-nums[j])){
+                    List<Integer> temp =new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(-nums[i]-nums[j]);
+                    Collections.sort(temp);
+                    if(!uniq.contains(temp)){
+                        ans.add(temp);
+                        uniq.add(temp);
                     }
+                }
+                else{
+                    set.add(nums[j]);
                 }
             }
         }
-        return removeDuplicate(listOfLists);
+        return ans;
     }
-    public static  List<List<Integer>> removeDuplicate(List<List<Integer>> listofLists) {
-        Set<List<Integer>> uniqueLists = new HashSet<>();
-        for(List<Integer> sorted : listofLists ){
-            sorted.sort(null);
-            if(!uniqueLists.contains(sorted)){
-                uniqueLists.add(sorted);
-            }
-        }
-        return new ArrayList<>(uniqueLists);
-    }
+
 
        
     public static void main(String[] args) {
