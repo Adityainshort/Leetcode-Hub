fb7a122db118c286fdbdfca099f80fbeee5fb887
Aditya Raj Singh <adityarajsinghhardoi@gmail.com>
2023-12-12 18:35:23 +0530

    added potd 12th dec

---
+++ Leetcode1464/leetcode1464.java
@@ -0,0 +1,10 @@
+package Leetcode1464;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+    }
+}
